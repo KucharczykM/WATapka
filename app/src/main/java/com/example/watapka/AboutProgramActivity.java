f4d4c44138f37bc6a7fc245870cc4f905a710172
Mateusz <mateuszk8432@gmail.com>
2019-04-05 03:18:54 +0200

    3Aktywności, działają, można kontynuować. Apka sypie błędy. 3:18 oczy wypalone od ekranu. Pokonałem to gówno XD

--- app/src/main/java/com/example/watapka/AboutProgramActivity.java
+++ app/src/main/java/com/example/watapka/AboutProgramActivity.java
@@ -1,4 +1,15 @@
 package com.example.watapka;
 
-class AboutProgramActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutProgramActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_program);
+    }
+
+
 }
--- app/src/main/java/com/example/watapka/act_start.java
+++ app/src/main/java/com/example/watapka/act_start.java
@@ -1,19 +1,24 @@
 package com.example.watapka;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 public class act_start extends AppCompatActivity {
+
+    private Handler mHandler = new Handler();
+    private Runnable run = new Runnable() {
+        @Override
+        public void run() {
+            startActivity(new Intent(act_start.this,act_filters.class));
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_start);
-    }
-
-    public void proces(View view){
-
+        mHandler.postDelayed(run,2000);
     }
 }
